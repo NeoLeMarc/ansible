36977dcfd0aedef252172faef9a7ae07d952dfde
Marcel Noe <marcel@marcel-noe.de>
2015-06-21 22:07:15 +0200

    works

--- update_debian.sh
+++ update_debian.sh
@@ -1,2 +1,3 @@
 ansible debian -i hosts -u root -f 3 -m apt -a update_cache=yes -v
 ansible debian -i hosts -u root -f 3 -m apt -a upgrade=dist -v
+ansible debian -i hosts -u root -f 3 -a "apt-get autoremove -y"
