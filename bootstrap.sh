8c50b29d34c255e901900702977704205e967b6e
Marcel Noe <marcel@marcel-noe.de>
2020-08-29 22:38:32 +0200

    bootstrap now uses normal host file and not specific one

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt -y install git python3 python3-pip
 pip3 install ansible
-ansible-pull -U https://citadel.noetech.net/stash/scm/an/ansible.git -i hosts_pull
+ansible-pull -U https://citadel.noetech.net/stash/scm/an/ansible.git -i hosts
