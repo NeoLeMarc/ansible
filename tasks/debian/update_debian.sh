8f2ccbd225452c14c0b2f5931e3fc217acbd891f
Marcel Noe <marcel@marcel-noe.de>
2020-09-09 22:51:20 +0200

    updated debian update scripts to new vms

--- tasks/debian/update_debian.sh
+++ tasks/debian/update_debian.sh
@@ -1,3 +1,3 @@
-ansible debian_server -i hosts -u root -f 20 -m apt -a update_cache=yes -v
-ansible debian_server -i hosts -u root -f 20 -m apt -a upgrade=dist -v
-ansible debian_server -i hosts -u root -f 20 -a "apt-get autoremove -y"
+ansible debian_servers -i hosts -u root -f 20 -m apt -a update_cache=yes -v
+ansible debian_servers -i hosts -u root -f 20 -m apt -a upgrade=dist -v
+ansible debian_servers -i hosts -u root -f 20 -a "apt-get autoremove -y"
