bd427bfbe041a6e5b7c51daee23909f66b15e01f
Marcel Noe <marcel@marcel-noe.de>
2022-12-25 09:49:13 +0100

    added vaultwarden redeploy

--- tasks/debian/update_debian.sh
+++ tasks/debian/update_debian.sh
@@ -2,3 +2,6 @@
 ansible debian_servers -i hosts -u root -f 20 -m apt -a update_cache=yes -v
 ansible debian_servers -i hosts -u root -f 20 -m apt -a upgrade=dist -v
 ansible debian_servers -i hosts -u root -f 20 -a "apt-get autoremove -y"
+
+# Re-deploy vaultwarden
+ansible-playbook -i hosts --limit=dockerserver tasks/bitwarden/vaultwarden.yml
